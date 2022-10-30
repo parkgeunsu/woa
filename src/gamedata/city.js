bff716276bd035228bf187c41b45cd8d3e0a7296
박근수 <goose@baggeunsuui-MacBookAir.local>
2022-10-30 19:24:49 +0900

    js

--- src/gamedata/city.js
+++ src/gamedata/city.js
@@ -1,6 +1,6 @@
 export const city = {
 	port:[
-		{idx:0,cityName:'Ganghwa',price:1,toolShop:[],tradingPost:[]}, 
+		{idx:0,cityName:'Ganghwa',mapIdx:0,position:[10,20],price:1,toolShop:{upgrade:[0,1,2,6,7,8],etc:[3,4,5,6,7,8,9,10,20,22]},tradingPost:[]}, 
 		{idx:1,cityName:'Ulsan',price:1,toolShop:[],tradingPost:[]},
 		{idx:2,cityName:'Pyongyang',price:1,toolShop:[],tradingPost:[]},
 		{idx:3,},
